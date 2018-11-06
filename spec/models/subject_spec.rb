99193a04a89a1f68c0357079a90816f7a60c9353
Amy Boyer <amy@zooniverse.org>
2018-11-06 15:19:35 -0600

    add method on subject to see if metadata header is set indicating this is a training subject

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Subject, type: :model do
+describe Subject, type: :model do
   describe '.update_cache' do
     it 'updates the cached metadata for a subject' do
       expect do
@@ -8,6 +8,19 @@ RSpec.describe Subject, type: :model do
       end.to change { Subject.count }.from(0).to(1)
 
       expect(Subject.first.metadata).to eq("biome" => "ocean")
+    end
+  end
+
+  describe '#training_subject?' do
+    it 'can tell if a subject is a training subject or not' do
+      expect((create :subject, metadata: {'#training_subject' => 'true'}).training_subject?).to be(true)
+      expect((create :subject, metadata: {'#training_subject' => 'false'}).training_subject?).to be(false)
+
+      expect((create :subject, metadata: {'#training_subject' => 'TRUE'}).training_subject?).to be(true)
+      expect((create :subject, metadata: {'#training_subject' => 'FALSE'}).training_subject?).to be(false)
+
+      expect((create :subject, metadata: {'#training_subject' => 'blargh'}).training_subject?).to be(false)
+      expect((create :subject, metadata: {'some_other_key' => 'FALSE'}).training_subject?).to be(false)
     end
   end
 end
